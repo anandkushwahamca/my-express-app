d44ea496d9fdea5ae0ccae6940b77e56bca81d28
anandkushwahamca <anand.kushwaha49@gmail.com>
2026-06-14 14:00:45 +0530

    creatte express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('This is working!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
